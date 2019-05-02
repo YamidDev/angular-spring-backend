2fa75103821541ce68103cd80c17d6ca46e48025
YamidDev <yamidcuetomazo@gmail.com>
2019-05-02 10:31:53 -0500

    Actualizacion de cambios

--- src/main/java/com/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -1,5 +1,7 @@
 package com.springboot.backend.apirest.controllers;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,10 +10,12 @@ import java.util.stream.Collectors;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -23,18 +27,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.springboot.backend.apirest.models.entity.Cliente;
 import com.springboot.backend.apirest.models.services.IClienteService;
+import com.springboot.backend.apirest.models.services.IUploadFileService;
 
-@CrossOrigin(origins= {"http://localhost:4200","http://localhost:8081","http://localhost"})
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/api/v1/")
 public class ClienteRestController {
 	
 	@Autowired
 	private IClienteService clienteService;
+	
+	@Autowired
+	private IUploadFileService uploadService;
+	
 	
 	@GetMapping("/clientes")
 	public List<Cliente> index(){
@@ -147,7 +158,13 @@ public class ClienteRestController {
 		
 		Map<String, Object> response = new HashMap<>();
 		
+		
 		try{
+			
+			Cliente cliente = clienteService.findById(id);
+			String nombreFotoAnt = cliente.getFoto();
+			
+			uploadService.eliminar(nombreFotoAnt);
 			
 			clienteService.delete(id);
 			
@@ -155,12 +172,59 @@ public class ClienteRestController {
 			
 			response.put("mensaje","Error al Eliminar el cliente");
 			response.put("error",e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
-			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-			
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);		
 		}
 		
 		response.put("mensaje", "El cliente ha sido eliminado con éxito!");
 		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
 		
+	}
+	
+	@PostMapping("/clientes/upload")
+	public ResponseEntity<?> upload(@RequestParam("archivo") MultipartFile archivo, @RequestParam("id") Long id){
+		Map<String, Object> response = new HashMap<>();
+		
+		Cliente cliente = clienteService.findById(id);
+		
+		if (!archivo.isEmpty()) {
+			String nombreArchivo = null;
+			try {	
+				nombreArchivo = uploadService.copiar(archivo);
+			} catch (IOException e) {
+				response.put("mensaje","Error al subir la imagen");
+				response.put("error",e.getMessage().concat(": ").concat(e.getCause().getMessage()));
+				return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+			
+			String nombreFotoAnt = cliente.getFoto();
+			
+			uploadService.eliminar(nombreFotoAnt);
+			
+			cliente.setFoto(nombreArchivo);
+			clienteService.save(cliente);
+			
+			response.put("cliente", cliente);
+			response.put("mensaje", "Has subido correctamente la imagen: "+ nombreArchivo);
+		}
+		
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/uploads/img/{nombreFoto:.+}")
+	public ResponseEntity<Resource> verFoto(@PathVariable String nombreFoto){
+		
+		Resource recurso = null;
+		
+		try {
+			recurso = uploadService.cargar(nombreFoto);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+		HttpHeaders cabecera = new HttpHeaders();
+		
+		cabecera.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" +recurso.getFilename() + "\"");
+		
+		return new ResponseEntity<Resource>(recurso, cabecera, HttpStatus.OK);
 	}
 }
--- src/main/java/com/springboot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/springboot/backend/apirest/models/entity/Cliente.java
@@ -5,10 +5,12 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -16,6 +18,8 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "clientes")
@@ -40,15 +44,17 @@ public class Cliente implements Serializable {
 	@Column(nullable=false, unique=true)
 	private String email;
 
-	@NotNull
+	@NotNull(message = "No puede estar vacío")
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
 	private Date createAt;
 	
-	@PrePersist
-	public void prePersit() {
-		createAt = new Date();
-	}
+	private String foto;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="region_id")
+	@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+	private Region region;
 	
 	public Long getId() {
 		return id;
@@ -89,6 +95,24 @@ public class Cliente implements Serializable {
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
 	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	
+	
+	public Region getRegion() {
+		return region;
+	}
+
+	public void setRegion(Region region) {
+		this.region = region;
+	}
+
 
 	private static final long serialVersionUID = 1L;
 }
