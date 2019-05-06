6f67eb35d243e2dadd58b4aae7a94bd3e8feb68d
YamidDev <yamidcuetomazo@gmail.com>
2019-05-06 15:48:01 -0500

    Actualización de cambios

---
+++ src/main/java/com/springboot/backend/apirest/auth/SpringSecurityConfig.java
@@ -0,0 +1,12 @@
+package com.springboot.backend.apirest.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private UserDetailsService
+}
